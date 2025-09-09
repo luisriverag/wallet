9c839f6ef76d194e6c726bfbe21d565d75f04bec
elvis <kurtnebiev-elvis4@gmail.com>
2025-09-09 17:48:05 +0300

    Refactor TabsAdapter to use FragmentFactory and improve notifications

--- mbw/src/main/java/com/mycelium/wallet/activity/modern/adapter/TabsAdapter.kt
+++ mbw/src/main/java/com/mycelium/wallet/activity/modern/adapter/TabsAdapter.kt
@@ -3,6 +3,7 @@ package com.mycelium.wallet.activity.modern.adapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayout
 import com.mycelium.wallet.MbwManager
@@ -10,6 +11,7 @@ import com.mycelium.wallet.MbwManager
 class TabsAdapter(val activity: AppCompatActivity, val mbwManager: MbwManager) :
     FragmentStateAdapter(activity) {
     private val mTabs = mutableListOf<TabInfo>()
+    private val fragmentFactory = FragmentFactory()
 
     private data class TabInfo(
         val clss: Class<*>,
@@ -23,28 +25,38 @@ class TabsAdapter(val activity: AppCompatActivity, val mbwManager: MbwManager) :
         val info = TabInfo(clss, args, tab.text.toString(), tabTag)
         tab.tag = info
         mTabs.add(info)
-        notifyDataSetChanged()
+        notifyItemInserted(mTabs.size - 1)
     }
 
     fun addTab(i: Int, tab: TabLayout.Tab, clss: Class<*>, args: Bundle, tabTag: String) {
         val info = TabInfo(clss, args, tab.text.toString(), tabTag)
         tab.tag = info
         mTabs.add(i, info)
-        notifyDataSetChanged()
+        notifyItemInserted(i)
     }
 
     fun removeTab(tabTag: String) {
-        mTabs.removeAll { it.tag == tabTag }
-        notifyDataSetChanged()
+        val index = mTabs.indexOfFirst { it.tag == tabTag }
+        if (index != -1) {
+            mTabs.removeAt(index)
+            notifyItemRemoved(index)
+        }
     }
 
     override fun getItemCount(): Int = mTabs.size
 
     override fun createFragment(position: Int): Fragment =
-        mTabs[position].let { Fragment.instantiate(activity, it.clss.name, it.args) }
+        mTabs[position].let {
+            fragmentFactory.instantiate(it.clss.classLoader!!, it.clss.name).apply {
+                arguments = it.args
+            }
+        }
 
     override fun getItemId(position: Int): Long =
-        mTabs[position].tag.hashCode().toLong()
+        generateStableId(mTabs[position].tag)
+
+    override fun containsItem(itemId: Long): Boolean =
+        mTabs.any { generateStableId(it.tag) == itemId }
 
     fun getPageTitle(position: Int): CharSequence? =
         mTabs[position].title
@@ -57,4 +69,6 @@ class TabsAdapter(val activity: AppCompatActivity, val mbwManager: MbwManager) :
 
 //    override fun getItemPosition(`object`: Any): Int =
 //        POSITION_NONE
+
+    private fun generateStableId(tag: String): Long = tag.fold(0L) { acc, char -> acc * 31 + char.code }
 }
